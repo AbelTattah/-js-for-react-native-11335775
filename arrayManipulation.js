3bc0b3aef16887e4cdcedb0cd5c1856a84d4ffe2
Abel Tattah <122866505+AbelTattah@users.noreply.github.com>
2024-05-16 23:13:30 +0000

    Initial Commit

---
+++ arrayManipulation.js
@@ -0,0 +1,14 @@
+const processArray = (array) => array.map((item)=>{return item%2==0?Math.pow(item,2):item*3})
+
+const formatArrayStrings = (arrayOfStrings,arrayOfNumbers) => {
+    if (arrayOfNumbers.length >= arrayOfStrings.length) {
+        return arrayOfStrings.map((item,index)=>{ return arrayOfNumbers[index]%2==0?item.toUpperCase():item.toLowerCase()})
+    }
+    else {
+        throw new Error("Length of the Array of numbers must be greater than or equal to the array of strings")
+    }
+}
+
+
+export { processArray, formatArrayStrings }
+
